2897f42cfd1a9230c88ddb4302231fc734a5d0ea
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-10-26 04:40:56 -0400

    adds market rate updating/insertion

--- coinwatch_api/lib/coinwatch_api/assets/assets.ex
+++ coinwatch_api/lib/coinwatch_api/assets/assets.ex
@@ -102,11 +102,88 @@ defmodule CoinwatchApi.Assets do
     ExchangePair.changeset(exchange_pair, %{})
   end
 
+  #Returns the current price for all supported markets in JSON.
+  #Some values may be out of date by a few seconds.
+  #### Data is in the form "EXCHANGE:PAIR" : PRICE in a list
+  def markets_prices_URL() do
+    "https://api.cryptowat.ch/markets/prices"
+  end
 
-  #use upsert (Repo.insert_or_update)?
-  #TODO get all exchangepairs and rates
-    # from cryptowatch api
-    #TODO upsert into DB
-  #TODO perform every x seconds?
+  # Returns the market summary for all supported markets.
+  # Some values may be out of date by a few seconds.
+  def markets_summary_URL() do
+    "https://api.cryptowat.ch/markets/summaries"
+  end
+
+  def get_decoded_URL(url) do
+    res = HTTPoison.get!(url)
+    Poison.decode(res.body)
+  end
+
+  def isolate_result(res) do
+    with {:ok, result} <- Map.fetch(res, "result") do
+      result
+    end
+  end
+
+  @doc """
+    Gets all markets and their prices. For each obtained data,
+    an ExchangePair is either updated or created to contain the latest
+    price information.
+  """
+  def upsert_exchange_pairs() do
+    with {:ok, map} <- get_decoded_URL(markets_prices_URL()) do
+      map
+      |> isolate_result()
+      |> Enum.each(fn {k, v} ->
+        upsert_pair(k, v)
+      end)
+    end
+  end
+
+
+  @doc """
+    Updates or inserts an ExchangePairs rate.
+    market is in the form "EXCHANGE:PAIR".
+  """
+  def upsert_pair(market, rate) do
+    list = String.split(market, ":")
+    exchange = List.first(list)
+    pair = List.last(list)
+
+    if String.length(pair) == 6 do
+      get_or_create(exchange, pair, rate)
+      |> ExchangePair.changeset(%{rate: rate})
+      |> Repo.insert_or_update()
+      |> broadcast_changes()
+    end
+  end
+
+  @doc """
+    Retrieves an existing ExchangePair matching the exchange and pair
+    arguments or creates a new one if nonexistent.
+  """
+  def get_or_create(exchange, pair, rate) do
+    case Repo.get_by(ExchangePair, exchange: exchange, pair: pair) do
+      nil -> # none exists
+        %ExchangePair{exchange: exchange, pair: pair, rate: rate}
+      expair ->
+        IO.puts(exchange <> ":" <> pair)
+        IO.inspect(rate)
+        IO.inspect(expair)
+        expair
+    end
+  end
+
+  @doc """
+    Broadcasts rate changes to users who follow the market.
+  """
+  def broadcast_changes(expair) do
+    #TODO for main app broadcast to users who follow expair
+    # FOR this experiment, inspect in console
+    expair
+  end
+
+
 
 end
