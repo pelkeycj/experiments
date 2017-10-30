55fdee9ac4c091b40c86e44e78ac1045c2b05db1
Jason <crouse.j@husky.neu.edu>
2017-10-30 15:24:44 -0400

    cleanup

--- email_notifs/lib/email_notifs/mailer.ex
+++ email_notifs/lib/email_notifs/mailer.ex
@@ -1,18 +1,28 @@
 defmodule EmailNotifs.Mailer do
   use Mailgun.Client,
       domain: Application.get_env(:email_notifs, :mailgun_domain),
-      key: Application.get_env(:email_notifs, :mailgun_key)#,
-      #mode: :test,
-      #test_file_path: "/tmp/mailgun.json"
+      key: Application.get_env(:email_notifs, :mailgun_key),
+      mode: :test,
+      test_file_path: "/tmp/mailgun.json"
 
   @from "coinwatch@mg.jdcrouse.org"
 
-
-  def send_price_alert_email(addr) do
-    send_email to: addr,
+  #
+  # the inputs are all the things we will store when a user requests updates
+  # **idea: pass these in some sort of structure that can be unpacked
+  #
+  # e_addr: email address of user
+  #   curr: symbol for currency that we are notifying them of
+  #    val: user-specified critical value as a string
+  def send_price_alert_email(e_addr, curr, val) do
+    send_email to: e_addr,
                from: @from,
-               subject: "Price Alert for BTCUSD",
-               text: "The price for BTCUSD has reached or exceeded your desired alert value of $6000! Sell now or regret not doing so."
+               subject: "Price Alert for " <> curr,
+               text: alert_text(curr, val)
+  end
+
+  defp alert_text(curr, val) do
+    "The price for " <> curr <> " has reached your desired alert value of " <> val <> "."
   end
 
 
