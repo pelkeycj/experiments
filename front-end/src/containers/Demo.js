03612075d028b8d5b05788c54a8e5d0f1ad49a60
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-11-02 02:59:35 -0400

    add TODOs to demo page

--- front-end/src/containers/Demo.js
+++ front-end/src/containers/Demo.js
@@ -3,10 +3,24 @@
  */
 
 import React from 'react';
+import Navigation from "../components/Navigation";
 
 class Demo extends React.Component {
     render() {
-        return <h1>Demo</h1>
+        return (
+            <div>
+                <Navigation/>
+                <div>
+                    <h1 style={{marginTop: '100px'}}>Demo page coming soon.</h1>
+                    <h3>TODOs: </h3>
+                    <ul>
+                        <li>create two sets of mock data to alternate between</li>
+                        <li>display mock data using cards?</li>
+                        <li>maintain a mock list of followed assetpairs so that they can be modified in demo?</li>
+                    </ul>
+                </div>
+            </div>
+        );
     }
 }
 
